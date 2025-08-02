0c3b4d6e2060dbe8f29617266d78e38f6c2f1cd0
Abinesh <63021688+abineshranganathan@users.noreply.github.com>
2025-08-02 15:55:27 +0000

    initial page

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,16 @@ function App() {
 
   return (
     <div className='inital-bg'>
-       <h1 className='title'>ABINESH R</h1>
+       <h1 className='name'>
+        <span>A</span>
+        <span>B</span>
+        <span>I</span>
+        <span>N</span>
+        <span>E</span>
+        <span>S</span>
+        <span>H</span>
+        <span>R</span>
+       </h1>
     </div>
    
   )
